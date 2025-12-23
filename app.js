2b1a93b1dde2ba022086b8f572cebd3f3b3e0da6
Sukh-Ochir927 <sukhochirbatjargal27@gmail.com>
2025-12-23 12:26:56 +0800

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const numbers = document.querySelector("#numbers");
 
+const body = document.querySelector("#dark");
+
+const container = document.querySelector("#container");
+
+const minus = document.querySelector("#minus");
+
+const sign = document.querySelector("#sign");
+
+console.log(sign);
+
 let num = 0;
 
 const increase = () => {
@@ -15,12 +25,32 @@ const decrease = () => {
   renderNum();
 };
 
+const minusIcon = () => {
+  if (num === 0) {
+    console.log(num);
+
+    minus.classList.add("display-block");
+  }
+};
+minusIcon();
+
 const renderNum = () => {
   numbers.innerHTML = num;
+  minusIcon();
 };
 
 const reset = () => {
   num = 0;
-  console.log(num);
+
   renderNum();
 };
+
+const toggle = () => {
+  container.classList.toggle("light");
+};
+
+// 2 dark bas ligth gesen 2 modetoi baina.
+// Tedgeeriig sar bolon narnii iconoor tolooluulj.
+// Default mode n har, saaral ongote bogood, default mode deer baih uyd UI deer narnii icon bairlana.
+// Tuhain narnii icon deer darah uyd tuhain icon sar bolon oorchlogdoj, ligth mode ruu shiljih buyu,
+// Tsagaaan tsaivari ongotei boloh ba ter uyd UI deer haragdah icon sarnii icon bolon oorchlogdnoo.
